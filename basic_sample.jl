982cc55c52964f7269097e45ec671767103a4c5b
isuke <isuke770@gmail.com>
2018-10-12 22:23:01 +0900

    Edit 'basic_sample.jl'

--- basic_sample.jl
+++ basic_sample.jl
@@ -1,3 +1,13 @@
+versioninfo() # =>
+# Julia Version 1.0.0
+# Commit 5d4eaca0c9 (2018-08-08 20:58 UTC)
+# Platform Info:
+#   OS: macOS (x86_64-apple-darwin14.5.0)
+#   CPU: Intel(R) Core(TM) i5-4250U CPU @ 1.30GHz
+#   WORD_SIZE: 64
+#   LIBM: libopenlibm
+#   LLVM: libLLVM-6.0.0 (ORCJIT, haswell)
+
 #
 # Type and Calculation
 #
@@ -93,3 +103,41 @@ findlast(iseven, [1, 2, 3, 2, 1])  # === findprev(iseven, [1, 2, 3, 2, 1], lasti
 split("Hello, Julia Lang!", [',', ' '], keepempty = false) # => ["Hello", "Julia", "Lang!"]
 replace("I use Python", "Python" => "Julia")
 'a' in "Julia"
+
+re = r"^\s*(#|$)"
+occursin(re, "# comment") # => true
+m = match(r"^\s*(?:#(.*)|$)", "#comment")
+m.match # => "#comment"
+m.captures[1] # => "comment"
+
+#
+# Pair and Dict
+#
+leadship = "Kagerou" => 1
+leadship[1] # => "Kagerou"
+leadship[2] # => 1
+
+kagerou = Dict("Kagerou" => 1, "Shiranui" => 2)
+kagerou["Kagerou"] # => 1
+get(kagerou, "Yukikaze", 8) # => 8
+haskey(kagerou, "Kagerou") # => true
+keys(kagerou) # => ["Kagerou", "Shiranui"]
+values(kagerou) # => [1, 2]
+
+#
+# REPL
+#
+1 + 1
+ans # => 2
+varinfo() # =>
+# name                    size summary
+# –––––––––––––––– ––––––––––– –––––––––––––––––––––––––––––––––
+# Base                         Module
+# Core                         Module
+# InteractiveUtils 160.652 KiB Module
+# Main                         Module
+# ans                  0 bytes Nothing
+# kagerou            487 bytes Dict{String,Int64} with 2 entries
+# leadship            31 bytes Pair{String,Int64}
+# m                  359 bytes RegexMatch
+# re                 137 bytes Regex

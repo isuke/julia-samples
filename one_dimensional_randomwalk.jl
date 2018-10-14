bafa3e7987ed05cc630d6fd6cadb52721e45f1de
isuke <isuke770@gmail.com>
2018-10-14 16:39:59 +0900

    Edit 'one_dimensional_randomwalk.jl'

--- one_dimensional_randomwalk.jl
+++ one_dimensional_randomwalk.jl
@@ -20,3 +20,12 @@ end
 
 N = 1000
 xs = zeros(Float64, N)
+
+nsteps = length(ARGS) > 0 ? parse(Int    , ARGS[1]) : 1000
+prob   = length(ARGS) > 1 ? parse(Float64, ARGS[2]) : 0.5
+
+result = randmwalk(nsteps, prob)
+
+for (t, x) in enumerate(result)
+  println("$(t-1) $x")
+end
